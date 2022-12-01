55b8b6687b4fd2786cedb8690f5e1123b8558e76
Steve <stephen.avsec@gmail.com>
2022-12-01 13:56:39 -0600

    Fixing some formatting

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -2,7 +2,10 @@ defmodule Aoc2022 do
 
   def parse_input(str) do
     mapped_split = fn(s) -> String.split(s, "\n", trim: true) end
-    str |> String.split("\n\n") |> Enum.map(mapped_split) |> Enum.map(fn (l) -> Enum.map(l, fn s -> String.to_integer(s) end) end)
+    str 
+    |> String.split("\n\n") 
+    |> Enum.map(mapped_split) 
+    |> Enum.map(fn (l) -> Enum.map(l, fn s -> String.to_integer(s) end) end)
   end
 
   def nlargest(lst, n) when n > 0 do
@@ -14,11 +17,18 @@ defmodule Aoc2022 do
   end
 
   def day1(input) do
-    input |> parse_input() |> Enum.map(fn l -> Enum.sum(l) end) |> Enum.max()
+    input 
+    |> parse_input() 
+    |> Enum.map(fn l -> Enum.sum(l) end) 
+    |> Enum.max()
   end
 
   def day1_part2(input) do
-    input |> parse_input() |> Enum.map(fn (l) -> Enum.sum(l) end) |> nlargest(3) |> Enum.sum()
+    input 
+    |> parse_input() 
+    |> Enum.map(fn (l) -> Enum.sum(l) end) 
+    |> nlargest(3) 
+    |> Enum.sum()
   end
 
 end
