3e29847f2a13cdb03a9548ede569e0f4dfe09a36
Steve <stephen.avsec@gmail.com>
2022-12-02 11:03:02 -0600

    Day 2 solutions

--- lib/aoc2022.ex
+++ lib/aoc2022.ex
@@ -16,6 +16,70 @@ defmodule Aoc2022 do
     []
   end
 
+  def rps_scorer(round) do
+    {opponent, player} = round
+    cond do
+      opponent == "A" and player == "X" -> "D"
+      opponent == "A" and player == "Y" -> "W"
+      opponent == "A" and player == "Z" -> "L"
+      opponent == "B" and player == "X" -> "L"
+      opponent == "B" and player == "Y" -> "D"
+      opponent == "B" and player == "Z" -> "W"
+      opponent == "C" and player == "X" -> "W"
+      opponent == "C" and player == "Y" -> "L"
+      opponent == "C" and player == "Z" -> "D"
+    end
+  end
+
+  def alt_rps_scorer(round) do
+    {opponent, outcome} = round
+    cond do
+      opponent == "A" and outcome == "X" -> "S"
+      opponent == "A" and outcome == "Y" -> "R"
+      opponent == "A" and outcome == "Z" -> "P"
+      opponent == "B" and outcome == "X" -> "R"
+      opponent == "B" and outcome == "Y" -> "P"
+      opponent == "B" and outcome == "Z" -> "S"
+      opponent == "C" and outcome == "X" -> "P"
+      opponent == "C" and outcome == "Y" -> "S"
+      opponent == "C" and outcome == "Z" -> "R"
+    end
+  end
+
+
+  def scorer(round) do
+    {_opponent, player} = round
+    outcome = rps_scorer(round)
+    score = cond do
+      outcome == "W" -> 6
+      outcome == "D" -> 3
+      outcome == "L" -> 0
+    end
+    cond do
+      player == "X" -> score + 1
+      player == "Y" -> score + 2
+      player == "Z" -> score + 3
+    end
+  end
+
+  def alt_scorer(round) do
+    {_opponent, outcome} = round
+    player = alt_rps_scorer(round)
+    score = cond do
+      outcome=="X" -> 0
+      outcome=="Y" -> 3
+      outcome=="Z" -> 6
+    end
+    cond do
+      player=="R" -> score+1
+      player=="P" -> score+2
+      player=="S" -> score+3
+    end
+  end
+
+
+
+
   def day1(input) do
     input 
     |> parse_input() 
@@ -28,6 +92,24 @@ defmodule Aoc2022 do
     |> parse_input() 
     |> Enum.map(fn (l) -> Enum.sum(l) end) 
     |> nlargest(3) 
+    |> Enum.sum()
+  end
+
+  def day2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s -> String.split(s, " ") end)
+    |> Enum.map(fn s -> List.to_tuple(s) end)
+    |> Enum.map(fn s -> scorer(s) end)
+    |> Enum.sum()
+  end
+
+  def day2_part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn s -> String.split(s, " ") end)
+    |> Enum.map(fn s -> List.to_tuple(s) end)
+    |> Enum.map(fn s -> alt_scorer(s) end)
     |> Enum.sum()
   end
 
--- test/aoc2022_test.exs
+++ test/aoc2022_test.exs
@@ -12,4 +12,14 @@ defmodule Aoc2022Test do
     day_1 = File.read!("data/day1.txt")
     assert Aoc2022.day1_part2(day_1) == 204639
   end
+
+  test "day 2 part 1" do
+    input = File.read!("data/day2.txt")
+    assert Aoc2022.day2(input) == 13446
+  end
+
+  test "day 2 part 2" do
+    input = File.read!("data/day2.txt")
+    assert Aoc2022.day2_part2(input) == 13509
+  end
 end
